a1cfb126b22afa60cbf8776ecf172e1f5ea7d3d0
Bawaw <balder.croquet@student.ucll.com>
2016-02-20 12:55:24 +0100

    Rotated Card pile, TODO: change dimensions

--- Whist-GUI/Converters/PileViewPortConverter.cs
+++ Whist-GUI/Converters/PileViewPortConverter.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Media;
 using Whist.GameLogic;
 using Whist.GameLogic.ControlEntities;
 using Whist_GUI.ViewLogic;
@@ -15,6 +16,9 @@ namespace Whist_GUI
 {
     public class ViewPortConverter : IMultiValueConverter
     {
+        public const float height = 145.2f;
+        public const float width = 100;
+
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {    
             var gvm = values[0] as BaseGameViewModel;
@@ -64,6 +68,41 @@ namespace Whist_GUI
                     return 0;
                 case 4:
                     return 1;
+                default:
+                    break;
+            }
+
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class ViewPortConverter3 : IMultiValueConverter
+    {
+        public const float HEIGHT = 145.2f;
+        public const float width = 100;
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var gvm = values[0] as BaseGameViewModel;
+            var viewport = values[1] as Viewport3D;
+            if (gvm == null || viewport == null) return null;
+
+            switch (gvm.CurrentPlayer.Number)
+            {
+                case 1:
+                    return new RotateTransform(0,0.5,0.5);
+                case 2:
+                    return new RotateTransform(90, 0.5, 0.5);
+                case 3:
+                    return new RotateTransform(0,0.5, 0.5);
+                case 4:
+                    return new RotateTransform(90, 0.5, 0.5);
                 default:
                     break;
             }
