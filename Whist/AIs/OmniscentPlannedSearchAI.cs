3a896fb1c7c065b5901179c94579fd4160d678a0
Bawaw <balder.croquet@student.ucll.com>
2016-02-28 10:12:49 +0100

    searchTree

---
+++ Whist/AIs/OmniscentPlannedSearchAI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Whist.Datastructures;
+using Whist.GameLogic;
+using Whist.GameLogic.ControlEntities;
+
+namespace Whist.AIs
+{
+    class PlaySearchAI
+    {
+
+        private GameManager gameManager;
+        private SearchTree<Card> searchTree;
+        private IReferee Referee;
+
+        //input: Card you want to play, output: Heuristic value
+        public int TestMove(Card Card, HandCollection[] cards, HandCollection PlayerCollection)
+        {
+            SearchTreeNode<Card> playedCards = new SearchTreeNode<Card>(Card);
+            for (int i = 0; i < 13; i++)
+            {
+                foreach (var hand in cards)
+                {
+                    var validCards = ValidateCards(Card, hand.Cards);
+                    
+                    foreach (var card in validCards)
+                    {
+                        playedCards.AddChild(card);
+                    }
+                    
+                }
+            }
+
+            return 0;
+        }
+
+        public IList<Card> ValidateCards(Card Lead, IList<Card> Cards)
+        {
+            if (Lead == null) return Cards;
+            return Cards.Where(x => Referee.ValidateMove(x, Lead, Cards.ToList())).ToList();
+        }
+
+    }
+}
---
+++ Whist/Datastructures/SearchTree.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whist.Datastructures
+{
+
+    public class SearchTree<T>
+    {
+        private SearchTreeNode<T> root;
+
+        public SearchTree()
+        {
+            root = null;
+        }
+
+        public virtual void Clear()
+        {
+            root = null;
+        }
+
+        public SearchTreeNode<T> Root
+        {
+            get
+            {
+                return root;
+            }
+            set
+            {
+                root = value;
+            }
+        }
+    }
+
+    public delegate void TreeVisitor<T>(T nodeData);
+
+    public class SearchTreeNode<T>
+    {
+        private T data;
+        private LinkedList<SearchTreeNode<T>> children;
+
+        public SearchTreeNode(T data)
+        {
+            this.data = data;
+            children = new LinkedList<SearchTreeNode<T>>();
+        }
+
+        public void AddChild(T data)
+        {
+            children.AddFirst(new SearchTreeNode<T>(data));
+        }
+
+        public void AddChild(SearchTreeNode<T> data)
+        {
+            children.AddFirst(data);
+        }
+
+
+        public SearchTreeNode<T> GetChild(int i)
+        {
+            foreach (SearchTreeNode<T> n in children)
+                if (--i == 0)
+                    return n;
+            return null;
+        }
+
+        public void Traverse(SearchTreeNode<T> node, TreeVisitor<T> visitor)
+        {
+            visitor(node.data);
+            foreach (SearchTreeNode<T> kid in node.children)
+                Traverse(kid, visitor);
+        }
+    }
+}
